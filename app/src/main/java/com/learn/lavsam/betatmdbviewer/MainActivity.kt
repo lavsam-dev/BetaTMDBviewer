6fb351fb072c7f36d5368a6a573f3c258c03ad55
lavsam-dev <lavsam@gmail.com>
2021-08-18 22:18:04 +0400

    Initial commit

---
+++ app/src/main/java/com/learn/lavsam/betatmdbviewer/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.learn.lavsam.betatmdbviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.learn.lavsam.betatmdbviewer.R
+import com.learn.lavsam.betatmdbviewer.databinding.MainActivityBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
